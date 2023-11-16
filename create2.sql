e00ee97540e799fcc8519005ef7ddaae04419b7c
frankie02m2023 <gustavonelson098@gmail.com>
2023-11-16 14:25:08 +0000

    Added on update cascade to both create files

--- create2.sql
+++ create2.sql
@@ -16,7 +16,7 @@ CREATE TABLE Subscriptions(
     subscription_start DATE NOT NULL,
     subscription_end DATE NOT NULL CHECK(subscription_end >= subscription_start),
     payment_method TEXT CHECK(payment_method = 'Credit Card' OR payment_method = 'Debit Card' OR payment_method = 'Paypal'),
-    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE
+    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Films_and_Series;
@@ -36,13 +36,13 @@ CREATE TABLE Films(
     film_id INTEGER PRIMARY KEY,
     film_duration_hours FLOAT NOT NULL,
     film_release_date DATE NOT NULL,
-    FOREIGN KEY(film_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE
+    FOREIGN KEY(film_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Series;
 CREATE TABLE Series(
     series_id INTEGER PRIMARY KEY,
-    FOREIGN KEY(series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE
+    FOREIGN KEY(series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Episode;
@@ -57,7 +57,7 @@ CREATE TABLE Episode(
     episode_description TEXT,
     episode_rating INTEGER CHECK(episode_rating > 0 AND episode_rating <= 5),
     UNIQUE (series_id,season_number,episode_number),
-    FOREIGN KEY(series_id) REFERENCES Series(series_id) ON DELETE CASCADE
+    FOREIGN KEY(series_id) REFERENCES Series(series_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Film_Views;
@@ -67,8 +67,8 @@ CREATE TABLE Film_Views(
     username TEXT NOT NULL,
     view_time DATETIME NOT NULL,
     UNIQUE(username,view_time),
-    FOREIGN KEY(film_id) REFERENCES Films(film_id) ON DELETE CASCADE,
-    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE
+    FOREIGN KEY(film_id) REFERENCES Films(film_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Series_Views;
@@ -78,8 +78,8 @@ CREATE TABLE Series_Views(
     username TEXT NOT NULL,
     view_time DATETIME NOT NULL,
     UNIQUE(username,view_time),
-    FOREIGN KEY(episode_id) REFERENCES Episode(episode_id) ON DELETE CASCADE,
-    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE
+    FOREIGN KEY(episode_id) REFERENCES Episode(episode_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Film_and_Series_Cast;
@@ -94,13 +94,13 @@ CREATE TABLE Film_and_Series_Cast(
 DROP TABLE IF EXISTS Actors;
 CREATE TABLE Actors(
     actor_id INTEGER PRIMARY KEY,
-    FOREIGN KEY(actor_id) REFERENCES Film_and_Series_Cast(cast_id) ON DELETE CASCADE
+    FOREIGN KEY(actor_id) REFERENCES Film_and_Series_Cast(cast_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Directors;
 CREATE TABLE Directors(
     director_id INTEGER PRIMARY KEY,
-    FOREIGN KEY(director_id) REFERENCES Film_and_Series_Cast(cast_id) ON DELETE CASCADE
+    FOREIGN KEY(director_id) REFERENCES Film_and_Series_Cast(cast_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Actor_Participations;
@@ -108,8 +108,8 @@ CREATE TABLE Actor_Participations(
     actor_id INTEGER NOT NULL,
     film_or_series_id INTEGER NOT NULL,
     PRIMARY KEY(actor_id,film_or_series_id),
-    FOREIGN KEY(actor_id) REFERENCES Actors(actor_id) ON DELETE CASCADE,
-    FOREIGN KEY(film_or_series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE
+    FOREIGN KEY(actor_id) REFERENCES Actors(actor_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(film_or_series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Director_Participations;
@@ -117,8 +117,8 @@ CREATE TABLE Director_Participations(
     director_id INTEGER NOT NULL,
     film_or_series_id INTEGER NOT NULL,
     PRIMARY KEY(director_id,film_or_series_id),
-    FOREIGN KEY(director_id) REFERENCES Directors(director_id) ON DELETE CASCADE,
-    FOREIGN KEY(film_or_series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE
+    FOREIGN KEY(director_id) REFERENCES Directors(director_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(film_or_series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Favourites_Film_and_Series;
@@ -126,8 +126,8 @@ CREATE TABLE Favourites_Film_and_Series(
     username TEXT NOT NULL,
     film_or_series_id INTEGER NOT NULL,
     PRIMARY KEY(username,film_or_series_id),
-    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE,
-    FOREIGN KEY(film_or_series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE
+    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(film_or_series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Favourites_Film_and_Series_Cast;
@@ -135,8 +135,8 @@ CREATE TABLE Favourites_Film_and_Series_Cast(
     username TEXT NOT NULL,
     cast_id INTEGER NOT NULL,
     PRIMARY KEY(username,cast_id),
-    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE,
-    FOREIGN KEY(cast_id) REFERENCES Film_and_Series_Cast(cast_id) ON DELETE CASCADE
+    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(cast_id) REFERENCES Film_and_Series_Cast(cast_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Reviews;
@@ -148,7 +148,7 @@ CREATE TABLE Reviews(
     review_description TEXT NOT NULL,
     review_rating INTEGER NOT NULL CHECK(review_rating > 0 AND review_rating <= 5),
     UNIQUE(username,review_time),
-    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE,
-    FOREIGN KEY(film_or_series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE
+    FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(film_or_series_id) REFERENCES Films_and_Series(film_or_series_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
